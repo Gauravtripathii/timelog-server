48b123d6f742d321f0d34a6843ec53038380a370
gauravtripathii <gauravtripathii7979@gmail.com>
2024-09-20 20:04:49 +0530

    init

---
+++ models/userModel.js
@@ -0,0 +1,30 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        required: [true, "Please provide a name"]
+    },
+    username: {
+        type: String,
+        required: [true, "Please provide a username"],
+        unique: true,
+    },
+    email: {
+        type: String,
+        required: [true, "Please provide an email"],
+        unique: true,
+    },
+    password: {
+        type: String,
+        required: [true, "Please provide a password"],
+    },
+    dob: {
+        type: Date
+    }
+});
+
+
+const User = mongoose.models.users || mongoose.model("users", userSchema);
+
+export default User;
